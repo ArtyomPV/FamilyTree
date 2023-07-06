1262d04751caf4df39be79723d12c2cd9b83b151
Artyom <itcoding76@gmail.com>
2023-07-06 20:00:45 +0200

    Создан атрибут для хранения членов семьи и конструктор класа

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -1,6 +1,12 @@
 import java.util.ArrayList;
 
 public class FamilyTree {
+    private ArrayList<Human> family;
+
+    public FamilyTree(ArrayList<Human> family) {
+        this.family = family;
+    }
+
 
 }
 
