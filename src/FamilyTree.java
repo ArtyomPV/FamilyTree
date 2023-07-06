505e7a9cf1d6bf038ba36eec8f05ec26409a92c9
Artyom <itcoding76@gmail.com>
2023-07-06 21:01:16 +0200

    не работает метод showHumanTree класса FamilyTree

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -6,17 +6,57 @@ public class FamilyTree {
     public FamilyTree() {
         this.family = new ArrayList<>();
     }
-    public ArrayList<Human> addHuman(Human human){
+    public void addHuman(Human human){
         this.family.add(human);
-        return this.family;
     }
     public void showFamilyMembers(){
-        for(Human human: this.family){
+        for (Human human : this.family) {
+            System.out.println("---------------");
             System.out.println(human);
         }
+
     }
-
-
+    public Human findByName(String name){
+        Human someone = null;
+        for (Human human : this.family) {
+            if(human.getFullName().equals(name)){
+                return human;
+            }
+        }
+        return null;
+    }
+    //TODO не работает метод
+     public void showHumanTree(String name){
+         Human someone = findByName(name);
+         Human mother = null;
+         Human father = null;
+         Human brother = null;
+         Human sister = null;
+         Human son = null;
+         Human dauther = null;
+        if(someone != null){
+            for (Human human:this.family) {
+                if(human.getMother().equals(someone.getFullName())){
+                    if (human.getGender().equals(Gender.Male)){
+                        son = human;
+                    } else {
+                        dauther = human;
+                    }
+                }
+            }
+            System.out.println(someone);
+            System.out.println(
+                    String.format(
+                            "Родители:\n\tмать:\t%s\n\tотец:\t%s",someone.getMother(), someone.getFather()
+                    ));
+            System.out.println(
+                    String.format(
+                            "Дети:\n\tСын:\t%s\n\tДочь:\t%s",son, dauther
+                    ));
+        } else {
+            System.out.println("Человека с таким именем нет");
+        }
+     }
 
 }
 
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
        FamilyTree myFamily = new FamilyTree();
        Human svetlana = new Human("Светлана Игорева", "1976-02-25", null, null, Gender.Female);
-       Human nikolay = new Human("Николай Петров", "1974-08-17", null, null, Gender.Female);
-       Human petr = new Human("Петр Петров", "1997-04-23", svetlana, nikolay, Gender.Female);
-       Human ivan = new Human("Иван Иванов", "1998-11-10", null, null, Gender.Female);
+       Human nikolay = new Human("Николай Петров", "1974-08-17", null, null, Gender.Male);
+       Human petr = new Human("Петр Петров", "1997-04-23", svetlana, nikolay, Gender.Male);
+       Human ivan = new Human("Иван Иванов", "1998-11-10", null, null, Gender.Male);
        Human anna = new Human("Анна Петрова", "2000-05-16", svetlana, nikolay, Gender.Female);
-       Human dima = new Human("Дима Иванов", "2020-12-05", anna, ivan, Gender.Female);
-       Human egor = new Human("Егор Иванов", "2022-07-12", anna, ivan, Gender.Female);
+       Human dima = new Human("Дима Иванов", "2020-12-05", anna, ivan, Gender.Male);
+       Human egor = new Human("Егор Иванов", "2022-07-12", anna, ivan, Gender.Male);
 
        myFamily.addHuman(svetlana);
        myFamily.addHuman(nikolay);
@@ -18,5 +18,7 @@ public class Main {
        myFamily.addHuman(egor);
 
        myFamily.showFamilyMembers();
+
+       myFamily.showHumanTree("Анна Петрова");
     }
 }
