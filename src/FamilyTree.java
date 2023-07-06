f22d4081bf327f06f5dc2b6ee538d47ee787095a
Artyom <itcoding76@gmail.com>
2023-07-06 20:01:55 +0200

    в класс FamilyTree добавлен метод добавления членов семьи

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -6,6 +6,11 @@ public class FamilyTree {
     public FamilyTree(ArrayList<Human> family) {
         this.family = family;
     }
+    public ArrayList<Human> addHuman(Human human){
+        this.family.add(human);
+        return this.family;
+    }
+
 
 
 }
